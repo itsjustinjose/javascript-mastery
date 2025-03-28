3153a023339e5433dbe5005510affb84d4af1452
itsjustinjose <itsjustinjose@gmail.com>
2025-03-28 12:41:08 +0000

    Functions

---
+++ app.js
@@ -0,0 +1,33 @@
+// function isValidPassword(password, username){
+//     if(password.length<8){
+//         return false;
+//     }
+//     if(password.indexOf(' ')!== -1){
+//         return false;
+//     }
+//     if(password.indexOf('username') !== -1){
+//         return false;
+//     }
+//     return true;
+// }
+
+function isValid(password, username) {
+  return (password.length < 8) &&
+    (password.indexOf(" ") !== -1) &&
+    (password.indexOf(username) !== -1);
+}
+
+console.log(isValid("udemdmbkfb1233", "justin"));
+
+// function isValid(password, username) {
+//     const tooShort = password.length < 8;
+//     const hasSpace = password.includes(" ");
+//     const hasUsername = password.indexOf(username) !== -1; // Checks if password contains username
+
+//     if (tooShort || hasSpace || hasUsername) {
+//       return false; // Fail if any condition is true
+//     }
+//     return true;
+//   }
+
+//   console.log(isValid("justin@123", "justin")); // Now returns `false`
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,25 @@
+// function isValidPassword(password, username){
+//     if(password.length<8){
+//         return false;
+//     }
+//     if(password.indexOf(' ')!== -1){
+//         return false;
+//     }
+//     if(password.indexOf('username') !== -1){
+//         return false;
+//     }
+//     return true;
+// }
+
+// function isValid(password, username) {
+//   if (
+//     password.length < 8 ||
+//     password.indexOf(" ") !== -1 ||
+//     password.indexOf("username") !== -1) {
+//     return false;
+//   }
+//   return true;
+// }
+
+
+// console.log(isValid("justin@123", "justin"));
