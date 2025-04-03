ba901aaa4fdd2ad6c5b302f43d0e6385df691b9b
itsjustinjose <itsjustinjose@gmail.com>
2025-04-03 23:53:09 +0100

    Added arrorw functions1

--- app.js
+++ app.js
@@ -40,4 +40,46 @@ sing();
 
 
 //Implicit return
-const nums = []
+const nums = [1,2,3,4,5,6];
+
+const doubles1 = nums.map(function(n){
+    return n*2
+})
+
+console.log(doubles1);
+
+const doubles2 = nums.map(n =>{
+    return n*2;
+})
+
+console.log(doubles2);
+
+
+const doubles3 = nums.map(n => n*2)
+
+console.log(doubles3);
+
+
+//condionals
+const parityList = nums.map(function(n){
+    if(n%2 === 0) {
+        return 'true';
+    } else {
+        return 'false';
+    }
+})
+console.log(parityList)
+
+const parityList1 = nums.map((n)=>{
+    if(n%2 === 0) {
+        return 'true';
+    } else {
+        return 'false';
+    }
+})
+console.log(parityList1)
+
+const parityList2 = nums.map((n)=>( n%2 === 0 ? 'true': 'false'))
+const parityList3 = nums.map((n)=> n%2 === 0 ? 'true': 'false')
+console.log(parityList2)
+console.log(parityList3)
